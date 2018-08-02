e19d40b4ee9103b73082d415019d7a39a3d01af9
Balázs Horváth <stadd@windowslive.com>
2018-08-02 17:10:29 +0200

    feat(IWontCheatOnTheExam): finish task

--- week-01/day-3/Workshop/IWontCheatOnTheExams/src/IWontCheatOnTheExams.java
+++ week-01/day-3/Workshop/IWontCheatOnTheExams/src/IWontCheatOnTheExams.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class IWontCheatOnTheExams {
+    public static void main(String[] args){
+
+        String a = "I won't cheat on the exam!";
+
+        for(int i = 0; i < 100; i++)
+            System.out.println(a);
+    }
 }
--- week-01/day-3/Workshop/PartyIndicator/src/PartyIndicator.java
+++ week-01/day-3/Workshop/PartyIndicator/src/PartyIndicator.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class PartyIndicator {
+    public static void main(String[] args){
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("First type the number of girls, then the boys who comes to the party!");
+
+        int userIn1 = sc.nextInt();
+        int userIn2 = sc.nextInt();
+
+        if(userIn1 == userIn2 && userIn1 + userIn2 >= 20){
+            System.out.println("The party is excellent!");
+        }else if(userIn1 + userIn2 >= 20 && userIn1 != userIn2){
+            System.out.println("Quite cool party!");
+        }else if(userIn1 + userIn2 < 20){
+            System.out.println("Average party...");
+        }else if(userIn1 == 0){
+            System.out.println("Sausage party");
+        }
+    }
 }
