47b6b8c95310c8ecb89b81491cbf5420b6a1dfec
Balázs Horváth <stadd@windowslive.com>
2018-08-02 13:49:18 +0200

    fix

--- week-01/day-3/Workshop/CodingHours/src/CodingHours.java
+++ week-01/day-3/Workshop/CodingHours/src/CodingHours.java
@@ -3,10 +3,10 @@ public class CodingHours {
 
         int hoursDay = 6;
         int weeks = 17;
-        double semester = weeks * hoursDay * 5;
-        double hoursWeekly = 52 *17;
+        double wHoursWeekly = 52;
+        double cHoursWeekly = hoursDay *5;
 
-        System.out.println(semester);
-        System.out.println(semester / hoursWeekly + "%");
+        System.out.println(hoursDay * 5 * weeks);
+        System.out.println(cHoursWeekly / wHoursWeekly +"%");
     }
 }
---
+++ week-01/day-3/Workshop/DefineBasicInfo/src/DefineBasicInfo.java
@@ -0,0 +1,5 @@
+public class DefineBasicInfo {
+    public static void main(String[] args){
+
+    }
+}
