90e227234591f30ffbe7afa2a0b5251c08cb388b
Balázs Horváth <stadd@windowslive.com>
2018-08-02 13:25:56 +0200

    added FavoriteNumber

---
+++ week-01/day-3/Workshop/FavoriteNumber/src/FavoriteNumber.java
@@ -0,0 +1,7 @@
+public class FavoriteNumber {
+    public static void main(String[] args){
+        int favNumber = 2;
+
+        System.out.println("My favorite number is: " + favNumber);
+    }
+}
