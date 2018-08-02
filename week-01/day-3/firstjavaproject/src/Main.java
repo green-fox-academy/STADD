03b9a90a682a8c6472b55ceb22784d2c35af0bb3
Balázs Horváth <stadd@windowslive.com>
2018-08-02 11:36:12 +0200

    added scanner

--- week-01/day-3/firstjavaproject/src/Main.java
+++ week-01/day-3/firstjavaproject/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        /*
+        int intNum = 0;
+        intNum++;
+        intNum += 5;        // *= , /=
+        double dub = 1.5;
+        boolean bool = true;
+
+
         System.out.println("Hello World");
+        */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Írd be a neved");
+        String név = sc.nextLine();
+        System.out.println("Hello " + név +"!");
+
     }
 }
