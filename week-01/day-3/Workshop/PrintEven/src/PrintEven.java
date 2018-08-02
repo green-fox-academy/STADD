f7356e8c3aea940c4e0dfe3a14a5be49e5f9c982
Balázs Horváth <stadd@windowslive.com>
2018-08-02 17:33:56 +0200

    feat(PrintEven): added but not finished

---
+++ week-01/day-3/Workshop/PrintEven/src/PrintEven.java
@@ -0,0 +1,13 @@
+public class PrintEven {
+    public static void main(String[] args){
+
+        int a = 0;
+
+        while(a % 2 == 0 && a < 500){
+            a++;
+            System.out.println(a);
+
+            //not finished
+        }
+    }
+}
