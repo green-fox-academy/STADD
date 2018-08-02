13395dea3d5304e885427ed654f9eebe12c35cdf
Balázs Horváth <stadd@windowslive.com>
2018-08-02 12:51:55 +0200

    modified gitignore

---
+++ week-01/day-3/Workshop/IntroduceYourself/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args){
+        System.out.println("Balázs Horváth");
+        System.out.println(19);
+        System.out.println(1.75);
+    }
+}
