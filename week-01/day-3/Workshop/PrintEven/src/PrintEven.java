57a307f1b345c1008fd8510906e04db431925dbe
Balázs Horváth <stadd@windowslive.com>
2018-08-03 08:43:47 +0200

    feat(PrintEven): finish task

--- week-01/day-3/Workshop/PrintEven/src/PrintEven.java
+++ week-01/day-3/Workshop/PrintEven/src/PrintEven.java
@@ -1,10 +1,5 @@
 public class PrintEven {
     public static void main(String[] args){
-        /*
-        int a = 0;
-        */
-
-        int a = 0;
 
         for (int b = 0; b <= 500; b++)
             if (b % 2 ==0){
