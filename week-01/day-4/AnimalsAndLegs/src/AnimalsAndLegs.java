70a90fe0c61c60bf35ae5fca3d9fde3e5b6bb0ed
Balázs Horváth <stadd@windowslive.com>
2018-08-03 20:28:57 +0200

    feat(AnimalsAndLegs): finish task

--- week-01/day-4/AnimalsAndLegs/src/AnimalsAndLegs.java
+++ week-01/day-4/AnimalsAndLegs/src/AnimalsAndLegs.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class AnimalsAndLegs {
-    public static
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of chickens");
+        int userIn1 = sc.nextInt();
+        System.out.println("Now the number of pigs");
+        int userIn2 = sc.nextInt();
+
+        System.out.println(userIn1 * 2 + userIn2 * 4);
+    }
 }
