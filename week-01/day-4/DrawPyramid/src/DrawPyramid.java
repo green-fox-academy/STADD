535de54c77ca01070970f0c78c0924e8fe608646
Balázs Horváth <stadd@windowslive.com>
2018-08-06 00:04:16 +0200

    feat(DrawPyramid): finish task

--- week-01/day-4/DrawPyramid/src/DrawPyramid.java
+++ week-01/day-4/DrawPyramid/src/DrawPyramid.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DrawPyramid {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type in a number");
+        int userIn = sc.nextInt();
+
+        for(int i=0;i<userIn;i++){
+
+            for(int j=0;j<userIn-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=userIn-i;k<=userIn;k++){
+                System.out.print("*");
+            }
+            for(int k=userIn-i;k<userIn;k++){
+                System.out.print("*");
+            }
+            for(int j=0;j<userIn-i;j++){
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
