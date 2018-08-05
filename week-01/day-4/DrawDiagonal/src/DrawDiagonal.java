7d36b442f37fb2c4a04e6fb3360348be1c74ec52
Balázs Horváth <stadd@windowslive.com>
2018-08-06 00:01:38 +0200

    feat(DrawDiagonal): finish task

--- week-01/day-4/DrawDiagonal/src/DrawDiagonal.java
+++ week-01/day-4/DrawDiagonal/src/DrawDiagonal.java
@@ -2,17 +2,26 @@ import java.util.Scanner;
 
 public class DrawDiagonal {
     public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int userIn = sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
+        String s = "";
 
-        System.out.println("Type in a number:");
-        int userIn = sc.nextInt();
-
-        String s = "%";
-        String space = " ";
-
-        for (int a = 0; a < userIn; a++){
-            for (int b = 0; a )
+        for (int i = 1; i <= userIn; i++) {
+            for (int j = 1; j <= userIn; j++) {
+                if(i == 1 || i == userIn){
+                    System.out.print("%");
+                }else if(j == 1 || j == userIn){
+                    System.out.print("%");
+                }else if (j > i){
+                    System.out.print(" ");
+                }else if(j == i){
+                    System.out.print(s + "%");
+                    s += " ";
+                }
+            }
+            System.out.println();
         }
+
     }
 }
