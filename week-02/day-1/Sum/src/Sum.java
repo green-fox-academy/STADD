170e98f65a97981ac9fdf109b29a9957e2aa4d59
Balázs Horváth <stadd@windowslive.com>
2018-08-06 15:45:27 +0200

    feat(Sum): finish task

--- week-02/day-1/Sum/src/Sum.java
+++ week-02/day-1/Sum/src/Sum.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Sum {
+    public static void main(String[] args){
+
+        int a = 5;
+        int sum = sum(a);
+        System.out.println(sum);
+    }
+
+    public static int sum(int a){
+
+        int sum = 0;
+
+        for (int i = 0; i <= a; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
 }
