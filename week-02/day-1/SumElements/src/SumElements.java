7fa58bf47ae5d2b61dabb0f3dde6667148745d29
Balázs Horváth <stadd@windowslive.com>
2018-08-06 17:02:49 +0200

    feat(SumElements): finish task

---
+++ week-02/day-1/SumElements/src/SumElements.java
@@ -0,0 +1,8 @@
+public class SumElements {
+    public static void main(String[] args){
+
+        int[] array = new int[]{54, 23, 66, 12};
+        int sum = array[1] + array[2];
+        System.out.println(sum);
+    }
+}
