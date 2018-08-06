07e67aeb08a413bdcb8edb8e72c8c47f51913a75
Balázs Horváth <stadd@windowslive.com>
2018-08-06 15:12:46 +0200

    feat(Doubling): finish task

--- week-02/day-1/Doubling/src/Doubling.java
+++ week-02/day-1/Doubling/src/Doubling.java
@@ -2,14 +2,11 @@ public class Doubling {
     public static void main(String[] args){
 
         int baseNum = 123;
-
         System.out.println(doubling(baseNum));
     }
 
     public static int doubling(int a){
-        int baseNum = 123;
-        int doubling = baseNum * 2;
-
-        return doubling;
+        return a *2;
     }
+
 }
