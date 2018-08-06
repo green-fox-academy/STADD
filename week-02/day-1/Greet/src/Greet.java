ced787ae5f769e964a5d5a333b6468957bf662d4
Balázs Horváth <stadd@windowslive.com>
2018-08-06 14:40:28 +0200

    feat(Greet): finish task

---
+++ week-02/day-1/Greet/src/Greet.java
@@ -0,0 +1,15 @@
+public class Greet {
+    public static void main(String[] args){
+
+        String al = "Greenfox";
+
+        System.out.println(greet(al));
+    }
+
+    public static String greet(String a){
+        String a1 = "Greenfox";
+        String greet = "Greetings dear, " + a1;
+
+        return greet;
+    }
+}
