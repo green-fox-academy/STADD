f2864d7e829d4ef5a50bd58c59d62a269c71fc5f
Balázs Horváth <stadd@windowslive.com>
2018-08-06 20:35:12 +0200

    feat(AppendA): finish task

---
+++ week-02/day-1/AppendA/src/AppendA.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class AppendA {
+    public static void main(String[] args){
+
+        String[] animals = new String[]{"koal", "pand", "zebr"};
+
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] = animals[i] + "a";
+            System.out.println(Arrays.toString(animals));
+            
+        }
+    }
+}
