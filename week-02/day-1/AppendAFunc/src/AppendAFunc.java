6b977dbb39255d59bb992680d5b42611e9f86b20
Balázs Horváth <stadd@windowslive.com>
2018-08-06 15:18:22 +0200

    feat(AppendAFunc): finish task

--- week-02/day-1/AppendAFunc/src/AppendAFunc.java
+++ week-02/day-1/AppendAFunc/src/AppendAFunc.java
@@ -2,14 +2,10 @@ public class AppendAFunc {
     public static void main(String[] args){
 
         String typo = "Chinchill";
-
         System.out.println(appendAFunc(typo));
     }
 
     public static String appendAFunc(String a){
-        String typo = "Chinchill";
-        String appendAFunc = typo + "a";
-
-        return appendAFunc;
+        return a + "a";
     }
 }
