88d23ec342086c311740d1aaaf1167dc00cca1dc
Balázs Horváth <stadd@windowslive.com>
2018-08-07 14:05:58 +0200

    feat(TodoPrint): finish task

--- week-02/day-2/TodoPrint/src/TodoPrint.java
+++ week-02/day-2/TodoPrint/src/TodoPrint.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class TodoPrint {
+    public static void main(String... args){
+        String todoText = " - Buy milk\n";
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+        StringBuffer a = new StringBuffer(todoText);
+        int index = 0;
+        a.insert(index, "My todo:\n");
+        int index2 = 21;
+        a.insert(index2," - Download games\n");
+        int index3 = 39;
+        a.insert(index3, "     - Diablo\n");
+
+        System.out.println(a);
+    }
 }
