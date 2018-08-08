8134fef0ba5ed7ac81ba658968d44dade04daf29
Balázs Horváth <stadd@windowslive.com>
2018-08-08 11:37:15 +0200

    feat(Drawing): finish task

---
+++ week-02/day-3/LineInTheMiddle/src/Drawing.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+    public static void mainDraw(Graphics graphics){
+        // draw a red horizontal line to the canvas' middle.
+        // draw a green vertical line to the canvas' middle.
+        int x = WIDTH / 2;
+        int y = HEIGHT / 2;
+        graphics.setColor(Color.red);
+        graphics.drawLine(0, y,320, y);
+        graphics.setColor(Color.green);
+        graphics.drawLine(x, 0, x, 343);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
