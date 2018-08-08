132ab0468d5891418cfcb1defb4d829321416495
Balázs Horváth <stadd@windowslive.com>
2018-08-08 11:51:03 +0200

    feat(ColoredBox): finish task

---
+++ week-02/day-3/ColoredBox/src/Drawing.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+        graphics.drawRect(10, 10, 100, 100);
+        graphics.setColor(Color.green);
+        graphics.drawLine(10, 10, 110, 10 );
+        graphics.setColor(Color.blue);
+        graphics.drawLine(110, 10, 110, 110);
+        graphics.setColor(Color.red);
+        graphics.drawLine(10, 110, 110, 110);
+        graphics.setColor(Color.orange);
+        graphics.drawLine(10, 110, 10, 10);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
