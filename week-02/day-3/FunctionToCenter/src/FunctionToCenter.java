89902c675fc600af19c15c43743a5f77237257dc
Balázs Horváth <stadd@windowslive.com>
2018-08-09 15:11:17 +0200

    feat(FunctionToCenter): finish task

--- week-02/day-3/FunctionToCenter/src/FunctionToCenter.java
+++ week-02/day-3/FunctionToCenter/src/FunctionToCenter.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class FunctionToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a line from that point to the center of the canvas.
+        // Draw 3 lines with that function. Use loop for that.
+
+        for (int x = 0; x < 320; x += 20) {
+            for (int y = 0; y <= 300; y += 20) {
+                if (x == 300 || y == 300 || x == 0 || y == 0) {
+                    lineDraw(graphics, x, y);
+                }
+            }
+        }
+
+    }
+
+    public static void lineDraw(Graphics graphics, int x, int y){
+        graphics.drawLine(x, y, 150, 147);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
 }
