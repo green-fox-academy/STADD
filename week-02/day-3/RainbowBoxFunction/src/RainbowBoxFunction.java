f78b04029f94cd88bc5ff0cc3e40b026ea51883b
Balázs Horváth <stadd@windowslive.com>
2018-08-09 17:09:55 +0200

    feat(RainbowBoxFunction): Finish task

--- week-02/day-3/RainbowBoxFunction/src/RainbowBoxFunction.java
+++ week-02/day-3/RainbowBoxFunction/src/RainbowBoxFunction.java
@@ -7,10 +7,6 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RainbowBoxFunction {
     public static void mainDraw(Graphics graphics) {
-        // Create a square drawing function that takes 2 parameters:
-        // The square size, and the fill color,
-        // and draws a square of that size and color to the center of the canvas.
-        // Create a loop that fills the canvas with rainbow colored squares.
 
         int x = 355;
         drawSquare(graphics, x);
