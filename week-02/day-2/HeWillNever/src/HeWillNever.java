01228b850a3dbe005cd64e6b2e6279a2fb57b397
Balázs Horváth <stadd@windowslive.com>
2018-08-09 17:03:48 +0200

    feat(HeWillNever): finish task

--- week-02/day-2/HeWillNever/src/HeWillNever.java
+++ week-02/day-2/HeWillNever/src/HeWillNever.java
@@ -1,12 +1,12 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class HeWillNever {
     public static void main(String... args){
         String out = "";
-        int[] notSoCrypticMessage = {1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11};
-
+        Integer[] notSoCrypticMessage = {1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11};
+        List<Integer> list = Arrays.asList(notSoCrypticMessage);
         Map<Integer, String> map = new HashMap<Integer, String>();
-
         map.put(7, "run around and desert you");
         map.put(50, "tell a lie and hurt you ");
         map.put(49, "make you cry, ");
@@ -15,15 +15,15 @@ public class HeWillNever {
         map.put(1, "Never gonna ");
         map.put(11, "\n");
         map.put(3, "say goodbye ");
-
-        for (int i = 1; i < notSoCrypticMessage.length; i++) {
-            for (Integer j : map.keySet()){
-                System.out.println(j);
-            }
-        }
         // Things are a little bit messed up
         // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
         // Assemble the fragments into the out variable
+
+        for (int i = 0; i < notSoCrypticMessage.length; i++) {
+            int x = list.get(i);
+            String value = map.get(x);
+            out = out + value;
+        }
 
         System.out.println(out);
     }
