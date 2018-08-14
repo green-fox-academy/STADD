3a9c902c08610ca7665dcfd75a39e00fc0963b8b
Balázs Horváth <stadd@windowslive.com>
2018-08-14 14:38:27 +0200

    feat(WriteMultipleLines): finish task && git push

---
+++ week-03/day-2/WriteMultipleLines/src/WriteMultipleLines.java
@@ -0,0 +1,33 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteMultipleLines {
+    public static void main(String[] args) {
+        // Open a file called "my-file.txt"
+        // Write your name in it as a single line
+        // If the program is unable to write the file,
+        // then it should print an error message like: "Unable to write file: my-file.txt"
+
+        String path = "my-file.txt";
+        String word = "apple";
+        int number = 5;
+        writeFile(path, word, number);
+    }
+
+    public static void writeFile(String path, String word, int number) {
+        List<String> content = new ArrayList();
+        for (int i = 0; i < number; i++) {
+            content.add(word);
+        }
+        try {
+            Path filePath = Paths.get(path);
+            Files.write(filePath, content);
+        } catch (Exception e) {
+            System.out.println("Unable to write file: my-file.txt");
+        }
+
+    }
+}
