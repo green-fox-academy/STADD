767cae6556bac5b4920099fefdad07582f212d07
Balázs Horváth <stadd@windowslive.com>
2018-08-15 15:58:11 +0200

    feat(Counter): finish task

--- week-03/day-3/Counter/src/Counter.java
+++ week-03/day-3/Counter/src/Counter.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Counter {
+    int counter = 0;
+
+    public void add(int number) {
+        this.counter += number;
+    }
+
+    public void add() {
+        this.counter++;
+    }
+
+    public void reset() {
+        this.counter = 0;
+    }
+
+    public void get() {
+        System.out.println(this.counter);
+    }
 }
