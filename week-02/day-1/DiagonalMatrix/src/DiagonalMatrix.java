bbf0d3fdd3172b89e33e544c1850ad0402d11d24
Balázs Horváth <stadd@windowslive.com>
2018-08-15 11:11:15 +0200

    not finished

--- week-02/day-1/DiagonalMatrix/src/DiagonalMatrix.java
+++ week-02/day-1/DiagonalMatrix/src/DiagonalMatrix.java
@@ -1,5 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class DiagonalMatrix {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userIn = sc.nextInt();
 
 
     }
--- week-03/day-2/TicTacToe/src/TicTacToe.java
+++ week-03/day-2/TicTacToe/src/TicTacToe.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class TicTacToe {
+
+    public static void main(String[] args) {
+        // Write a function that takes a filename as string,
+        // open and read it. The file data represents a tic-tac-toe
+        // game result. Return 'X'/'O'/'draw' based on which player
+        // has winning situation.
+        String s = "win";
+        ticTacResult(s);
+
+        System.out.println(ticTacResult("win-o.txt"));
+        // should print O
+
+        System.out.println(ticTacResult("win-x.txt"));
+        // should print X
+
+        System.out.println(ticTacResult("draw.txt"));
+        // should print draw
+    }
+
+    public static Boolean ticTacResult(String s) {
+        Path winO = Paths.get("win-o.txt");
+        Path winX = Paths.get("win-x.txt");
+        Path draw = Paths.get("draw.txt");
+
+        try {
+            List<String> wino = Files.readAllLines(winO);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ticTacResult(s);
+    }
 }
