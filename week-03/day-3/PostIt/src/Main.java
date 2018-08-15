e668bb115519ef64de42e58f38eee8c3cb2c61a8
Balázs Horváth <stadd@windowslive.com>
2018-08-15 13:12:10 +0200

    feat(PostIt): finish task

---
+++ week-03/day-3/PostIt/src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PostIt posIt1 = new PostIt("orange", "Idea 1", "blue");
+        PostIt postIt2 = new PostIt("pink", "Awesome", "black");
+        PostIt postIt3 = new PostIt("yellow", "Superb!", "green");
+    }
+}
---
+++ week-03/day-3/PostIt/src/PostIt.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class PostIt {
+    String background;
+    String text;
+    String textColor;
+
+    PostIt(String background, String text, String textColor) {
+        this.background = background;
+        this.text = text;
+        this. textColor = textColor;
+    }
+}
