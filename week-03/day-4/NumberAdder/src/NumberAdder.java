d8339677a1e24ab6ca7a306f91a8d798f9ce612f
Balázs Horváth <stadd@windowslive.com>
2018-08-20 15:18:34 +0200

    feat(NumberAdder): finished task

--- week-03/day-4/NumberAdder/src/NumberAdder.java
+++ week-03/day-4/NumberAdder/src/NumberAdder.java
@@ -1,7 +1,7 @@
 public class NumberAdder {
     public static void main(String[] args) {
 
-        System.out.println(adder(4));
+        System.out.println(adder(5));
     }
 
     public static int adder(int n) {
