625f51b7d6d4c949c8d5f3092989442081de76e4
Balázs Horváth <stadd@windowslive.com>
2018-08-23 12:45:02 +0200

    feat(Apple): finish task

---
+++ week-04/day-4/Apples/Tests/AppleTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleTest {
+
+    private Apple apple;
+
+    @Before
+    public void createApple() {
+        apple = new Apple();
+    }
+
+
+    @Test
+    public void getApple() {
+        assertEquals("apple", apple.getApple());
+    }
+}
---
+++ week-04/day-4/Apples/src/Apple.java
@@ -0,0 +1,11 @@
+public class Apple {
+    private String apple;
+
+    public Apple() {
+        this.apple = "apple";
+    }
+
+    public String getApple() {
+        return apple;
+    }
+}
