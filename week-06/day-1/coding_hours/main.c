aaeb6e96d6b10fb74047c3009dabf6c94db2be34
Balázs Horváth <stadd@windowslive.com>
2018-09-10 10:33:26 +0200

    feat(coding_hours): finish task

---
+++ week-06/day-1/coding_hours/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+  int hours_daily = 6;
+  int weeks = 17;
+  double average_weekly = 52;
+  double hours_weekly = hours_daily * 5;
+
+  printf("%d\n", hours_daily * weeks * 5);
+  printf("%.2f", hours_weekly / average_weekly);
+}
