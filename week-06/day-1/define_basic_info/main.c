96bb70a1efb7fb7ea4a752fdf7404b27079a68ba
Balázs Horváth <stadd@windowslive.com>
2018-09-10 11:04:39 +0200

    feat(define_basic_info): finish task

---
+++ week-06/day-1/define_basic_info/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    char first_letter[2] = "B";
+    int age = 19;
+    double height = 1.78;
+    int fav_number = 2;
+
+    printf("%s\n", first_letter);
+    printf("%d\n", age);
+    printf("%.2f\n", height);
+    printf("%d\n", fav_number);
+}
