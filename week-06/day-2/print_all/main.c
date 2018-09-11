54606dd7cc033cf568c053c017f5a8e7cdb5796d
Balázs Horváth <stadd@windowslive.com>
2018-09-11 09:47:09 +0200

    feat(print_all): finish task

---
+++ week-06/day-2/print_all/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numbers[] = {4, 5, 6, 7};
+    int i;
+
+    for (i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++)
+    {
+        printf("%d\n", numbers[i]);
+    }
+
+    return 0;
+}
