75c70a88b7253cd539b84643489ee80bc9169a04
Balázs Horváth <stadd@windowslive.com>
2018-09-14 08:06:35 +0200

    feat(distance): finish task

--- week-06/day-4/distance/main.c
+++ week-06/day-4/distance/main.c
@@ -4,27 +4,33 @@
 // create a function which takes a char array as a parameter and
 // returns the distance between the first and last occurance of character 's'
 
+int distance(char* array);
+
 int main ()
 {
     char str[] = "This is a sample string";
-    char *first, *last;
-    distance(str);
+    //char *first, *last;
+    printf("%d", distance(str));
 
     return 0;
 }
 
 int distance(char* array)
 {
-    int first;
-    int last;
     int i;
-    int* num;
+    int first = 0;
+    int last = 0;
 
-    for(i = 0; sizeof(array); i++) {
+    for(i = 0; i < 30; i++) {
         if(array[i] == 's') {
-            strcpy(num, array[i]);
-            //printf("%d\n", i);
-            printf("%d\n", num[i]);
+            first = i;
+            break;
+        }
+    }
+
+    for(i = 0; i < 30; i++) {
+        if(array[i] == 's') {
+            last += i;
         }
     }
 
