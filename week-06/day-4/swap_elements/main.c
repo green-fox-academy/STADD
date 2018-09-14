0ea632a246022cb0d9185c3335bdfb44ec40a1ee
Balázs Horváth <stadd@windowslive.com>
2018-09-14 18:45:41 +0200

    feat(swap_elements): works without function

---
+++ week-06/day-4/swap_elements/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc` by using a function
+// - That function should take an array as a parameter and return the correct array
+
+int main()
+{
+    char* abc[3] = {"first", "second", "third"};
+
+    printf("%s %s %s\n", abc[0], abc[1], abc[2]);
+
+    char* temp;
+
+    temp = abc[0];
+
+    abc[0] = abc[2];
+    abc[2] = temp;
+
+    printf("%s %s %s\n", abc[0], abc[1], abc[2]);
+
+
+    return 0;
+}
