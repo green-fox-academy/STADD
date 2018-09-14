81ed64d43536d40bc20ccf54b9f3efb489445ca4
Balázs Horváth <stadd@windowslive.com>
2018-09-14 14:56:32 +0200

    feat(colors): finish task

--- week-06/day-3/append_a/main.c
+++ week-06/day-3/append_a/main.c
@@ -9,28 +9,14 @@ char* append(char* name);
 
 int main()
 {
-    char *animals[100];
+    char animals[100];
 
     animals[0] = "koal";
     animals[1] = "pand";
     animals[2] = "zebr";
 
-    printf("%s\n", append(animals));
+    char *teszt = strtok(animals, " ");
+    printf("%s", teszt);
 
-    //printf("%s\n", append(p_animals));
     return 0;
-}
-
-char* append(char* name)
-{
-    int i;
-    size_t len = strlen(name);
-
-    for(i = 0; i < 10; i++) {
-        char *name2 = malloc(len + 2);
-        strcpy(name2[i], name[i]);
-        name2[len] = 'a';
-        name2[len + 1] = '\0';
-        return name2;
-    }
 }
